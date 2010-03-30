5892457ca056f18e7016432ff3b77aab4f1c9d3a
Gilles MATHURIN <gillesmath@macmini.local>
2010-03-30 15:22:07 -0400

    Fixed payment_notification.rb

--- app/models/payment_notification.rb
+++ app/models/payment_notification.rb
@@ -7,7 +7,7 @@ class PaymentNotification < ActiveRecord::Base
   
   def mark_subscription_as_verified
     if status == "verified"
-      subscription.update_attributes(:start_date => Time.parse(self.params[:subscr_date]), :trial_expiration_date => Time.parse(self.params[:subscr_date] + 1.month, :status => "verified", :subscriber_id => self.params[:subscr_id])
+      subscription.update_attributes(:start_date => Time.parse(self.params[:subscr_date]), :trial_expiration_date => Time.parse(self.params[:subscr_date] + 1.month), :status => "verified", :subscriber_id => self.params[:subscr_id])
     end
   end
 end
